f8df72503edb872f19d176998ebae3f69d731272
trev00 <62571368+trev00@users.noreply.github.com>
2025-01-21 19:50:29 -0500

    Update main.js

--- main.js
+++ main.js
@@ -32,13 +32,13 @@ groundMesh.castShadow = false;
 groundMesh.receiveShadow = true;
 scene.add(groundMesh);
 
-const spotLight = new THREE.SpotLight(0xffffff, 1);
+const spotLight = new THREE.SpotLight(0xffffff, 3);
 spotLight.position.set(0, 25, 0);
 spotLight.castShadow = true;
 spotLight.shadow.bias = -0.0001;
 scene.add(spotLight);
 
-const ambientLight = new THREE.AmbientLight(0x404040, 1);
+const ambientLight = new THREE.AmbientLight(0x404040, 3);
 scene.add(ambientLight);
 
 const axesHelper = new THREE.AxesHelper(2); // Size of 2 units
