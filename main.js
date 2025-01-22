4587f581e55d78505d9eb42062f78c7f15b83434
trev00 <62571368+trev00@users.noreply.github.com>
2025-01-21 20:42:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ groundMesh.castShadow = false;
 groundMesh.receiveShadow = true;
 scene.add(groundMesh);
 
-const spotLight = new THREE.SpotLight(0xffffff, 1000, 100, 0.22, 1);
+const spotLight = new THREE.SpotLight(0xffffff, 100, 100, 0.22, 1);
 spotLight.position.set(0, 25, 0);
 spotLight.castShadow = true;
 spotLight.shadow.bias = -0.0001;
