b1ba3ecfd7b022dd389fa83c26d68f4dc80f7670
trev00 <62571368+trev00@users.noreply.github.com>
2025-02-21 16:14:17 -0500

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Leva } from "leva";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <><Leva /><App /></>
+);
